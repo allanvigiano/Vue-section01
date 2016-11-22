26d7adb5f00b8a8a7e2c83fa1bd023ecab71ebed
Allan de Almeida Vigiano <allanvigiano@gmail.com>
2016-11-22 15:06:57 -0200

    Lesson 2.11

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 new Vue ({
     el: "#app",
     data: {
-        "title": "Hello World"
+        title: "Hello World"
     },
     methods: {
         sayHello: function () {
-            return 'Hello'
+            return this.title;
         }
     }
 });
