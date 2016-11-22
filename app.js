10d20268f71e58987911ea8a3b48fe4c41a8af11
Allan de Almeida Vigiano <allanvigiano@gmail.com>
2016-11-22 17:35:21 -0200

    Lesson 2.20

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ new Vue ({
     el: "#app",
     data: {
         counter: 0,
+        x: 0,
+        y: 0,
     },
     methods: {
         increase: function () {
             return this.counter ++;
+        },
+        updateCoordinates: function (event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
         }
     }
 });
